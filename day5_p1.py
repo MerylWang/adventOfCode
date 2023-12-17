f158ae5daa7f7bb546b987d3f81bcc9949842386
MerylWang <wangmeryl77@gmail.com>
2023-12-17 17:20:15 -0500

    day 6 wip

---
+++ day5_p1.py
@@ -0,0 +1,90 @@
+# basically: how many ms should you spend holding button? 
+# let t = time allowed for current race 
+# for i in 1...t, distance travelled = (t - k) * k, where k = ms spent holding button = speed 
+
+"""
+Determine the number of ways you could beat the record in each race. 
+What do you get if you multiply these numbers together?
+
+Time:      7  15   30
+Distance:  9  40  200
+"""
+
+import math
+
+
+example_input = """Time:      7  15   30
+Distance:  9  40  200"""
+
+example_arr = example_input.split("\n")
+
+"""
+- start with brute force? 
+
+lower - you need to go at least 9mm in at most 7ms; 9/7 = ceil(1.28) = 2 mm/ms is lowest speed required 
+ - ceil(distance/time) = min_k
+   - k should only be higher than this, since time is cut short by k 
+higher - you run out of time to travel, despite high speed 
+  - max possible k = time allowed - 1
+  
+
+"""
+
+def part_1(arr):
+    race_time_limits = arr[0].split(":")[1].strip().split()
+    race_record_dist = arr[1].split(":")[1].strip().split()
+
+    if len(race_time_limits) != len(race_record_dist):
+        raise Exception("Input not formatted correctly")
+    
+    n = len(race_time_limits)
+    res = 1
+
+    # for each race 
+    for i in range(n):
+        t, d = int(race_time_limits[i]), int(race_record_dist[i])
+        lower = math.ceil(d/t)
+        higher = t - 1
+
+        # find lower range of k 
+        while lower < t:
+            if (t - lower) * lower >= d:
+                print("race ", i, ' lower: ', lower)
+                break
+            else: 
+                lower += 1
+
+        # find higher range of k
+        while higher > 0:
+            if (t - higher) * higher >= d:
+                print("race ", i, ' higher: ', higher)
+                break
+            else: 
+                higher -= 1
+
+        # number of possible ways = higher - lower + 1
+        num_ways = higher - lower + 1
+        print("race ", i, ' num_ways: ', num_ways)
+
+
+        # multiply this for each race 
+        res *= num_ways
+
+    return res 
+
+# expected: 288 (4 * 8 * 9)
+print(part_1(example_arr))
+'''
+race  0  lower:  2
+race  0  higher:  5
+race  0  num_ways:  4
+race  1  lower:  4
+race  1  higher:  11
+race  1  num_ways:  8
+race  2  lower:  10
+race  2  higher:  20
+race  2  num_ways:  11
+352
+'''
+# race 3 is wrong - 
+# todo: find lower and higher manually 
